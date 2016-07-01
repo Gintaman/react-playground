36f81c01a0e52147713506b7c58dc8e9bca98d54
Jeff <jeff@mechazawa.gtfo>
2016-07-01 16:14:35 -0700

    ended at tutorial 5

--- public/main.js
+++ public/main.js
@@ -1,12 +1,49 @@
-var CommentBox = React.createClass({
+let Comment = React.createClass({
 	render: function() {
-		return(
-			<div className="commentBox">
-				Hello, world! I am a CommentBox.
+		return (
+			<div className="comment">
+				<h2 className="commentAuthor">
+					{this.props.author}
+				</h2>
+					{this.props.children}
 			</div>
 		);
 	}
 });
+
+let CommentList = React.createClass({
+	render: function() {
+		return (
+			<div className="commentList">
+				<Comment author="Pete Hunt">This is one comment.</Comment>
+				<Comment author="Jordan Walke">This is another comment.</Comment>
+			</div>
+		);
+	}
+});
+
+let CommentForm = React.createClass({
+	render: function() {
+		return (
+			<div className="commentForm">
+				Hello, world! I am a CommentForm.
+			</div>
+		);
+	}
+});
+
+let CommentBox = React.createClass({
+	render: function() {
+		return (
+			<div className="commentBox">
+				<h1>Comments</h1>
+				<CommentList></CommentList>
+				<CommentForm></CommentForm>
+			</div>
+		);
+	}
+});
+
 ReactDOM.render(
 	<CommentBox></CommentBox>,
 	document.getElementById('content')
